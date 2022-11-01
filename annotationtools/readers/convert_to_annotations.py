2c0e278db9d937f8172bd4f061100ec3c910f638
tomaszfurmanek <88382277+tomaszfurmanek@users.noreply.github.com>
2022-11-01 20:01:35 +0100

    Update convert_to_annotations.py

--- annotationtools/readers/convert_to_annotations.py
+++ annotationtools/readers/convert_to_annotations.py
@@ -346,21 +346,21 @@ class work_reader (object):
             i = 0
 	    
             schoolRep=[None]
-	    parseelement=0
-	    try:
+            parseelement=0
+            try:
                 doc['regionInterpretation']['schoolInterpretation']['schoolMaskRep']
                 parseelement = 1
             except :
-		try:
+                try:
                     doc['regionInterpretation']['schoolInterpretation']['schoolRep']
                     parseelement = 2
             	except :
                     parseelement = 0
                     print("ERROR: LSSS work file :: in  Procesing the information for the schools (null)")
-	            
+                    
             if parseelement==1 and not not(doc['regionInterpretation']['schoolInterpretation']['schoolMaskRep']):
                 schoolRep=doc['regionInterpretation']['schoolInterpretation']['schoolMaskRep']
-	    if parseelement==2 and not not(doc['regionInterpretation']['schoolInterpretation']['schoolRep']):
+            if parseelement==2 and not not(doc['regionInterpretation']['schoolInterpretation']['schoolRep']):
                 schoolRep=doc['regionInterpretation']['schoolInterpretation']['schoolRep']
 		    
 
