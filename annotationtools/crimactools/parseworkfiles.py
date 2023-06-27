0e7af093538a9643db55f4b03e9f0d9bf700c68a
tomaszfurmanek <88382277+tomaszfurmanek@users.noreply.github.com>
2023-06-27 16:56:41 +0200

    Update parseworkfiles.py : added reading ping_time from sv.zarr
    added reading ping_time from sv.zarr

--- annotationtools/crimactools/parseworkfiles.py
+++ annotationtools/crimactools/parseworkfiles.py
@@ -25,14 +25,15 @@ import math
 from numcodecs import Blosc
 import os
 import sys
-
+import pandas as pd
 
 class ParseWorkFiles:
-    def __init__(self, rawdir="", workdir="", pq_filepath=""):
+    def __init__(self, rawdir="", workdir="", pq_filepath="",svzarr_file=""):
         self.rawdir = rawdir
         self.workdir = workdir
         self.pq_filepath = pq_filepath
         self.pq_writer = None
+        self.svzarr_file = svzarr_file
         self.counter = 0
 
     def append_to_parquet(self,df, pq_filepath, pq_obj=None):
@@ -58,44 +59,75 @@ class ParseWorkFiles:
         pq_obj.write_table(table=pa_tbl)
         return pq_obj
 
+    def make_pingtime_rawfile_parquet(self, zarrfile ):
+        z2 = xr.open_zarr(zarrfile)
+        print(z2)
+        print(z2.ping_time)
+        
+        raw_file_array =  z2.raw_file.values
+        #ping_time_array = z2.ping_time.values.astype('datetime64[ns]') 
+        #print(ping_time_array)
+        ping_time_array =z2.ping_time.values.astype('datetime64[ns]') 
+         
+        print(ping_time_array)
+        # Create a pandas DataFrame
+        df = pd.DataFrame({'raw_file': raw_file_array, 'ping_time': ping_time_array})
+        
+        # Convert DataFrame to PyArrow Table
+        table = pa.Table.from_pandas(df)
+        
+        # Define the Parquet file path
+        parquet_file = zarrfile.replace("_sv.zarr", "_ping_time-raw_file.parquet")  
+        pq.write_table(table, parquet_file, coerce_timestamps='us', allow_truncated_timestamps=True )
 
 
 
 
     def run(self):
-        filenames = os.listdir(self.rawdir)
+        filenames = os.listdir(self.workdir)
         sorted_filenames = sorted(filenames)
-
+        
+        if self.svzarr_file.endswith(".zarr"):
+            self.make_pingtime_rawfile_parquet( self.svzarr_file )
+        
         for filename in sorted_filenames:
-            raw_fname=os.path.join(self.rawdir, filename)
-            if raw_fname.endswith(".raw"):
-                work_fname =os.path.join(self.workdir, filename.replace("raw","work"))
+            work_fname=os.path.join(self.workdir, filename)
+            if work_fname.endswith(".work"):
+                print (filename)
+                raw_fname =os.path.join(self.workdir, filename.replace("work","raw"))
                 exists_work = os.path.isfile(work_fname)
+                exists_raw = os.path.isfile(raw_fname)
                 if exists_work:
-                    idx_fname =   raw_fname.replace("raw","idx")
+                    idx_fname =   raw_fname.replace(".raw",".idx")
                     exists_idx = os.path.isfile(idx_fname)
-                    if exists_idx:
-                        ann_obj = None
-                        try:
-                            work = readers.work_reader( work_fname )
-                            ann_obj = readers.work_to_annotation(work, idx_fname)
-                        except Exception as e:
-                            exception_type, exception_object, exception_traceback = sys.exc_info()
-                            filename = exception_traceback.tb_frame.f_code.co_filename
-                            line_number = exception_traceback.tb_lineno
-                            print("Exception type: ", exception_type)
-                            print("File name: ", filename)
-                            print("Line number: ", line_number)
-                            # print("ERROR3: - Something went wrong when reading the WORK file: " + str(work_fname) + " (" + str( e) + ")")
+                    
+                    ann_obj = None
+                    try:
+                        work = readers.work_reader( work_fname )
+                        if self.svzarr_file.endswith(".zarr"):
+                            print(work_fname)
+                            print(self.svzarr_file)
+                            ann_obj = readers.work_to_annotation(work, idx_fname ,self.svzarr_file,False)
+                        else:
+                            if exists_idx:
+                                if exists_raw:
+                                    ann_obj = readers.work_to_annotation(work, idx_fname )
+                    except Exception as e:
+                        exception_type, exception_object, exception_traceback = sys.exc_info()
+                        filename = exception_traceback.tb_frame.f_code.co_filename
+                        line_number = exception_traceback.tb_lineno
+                        print("Exception type: ", exception_type)
+                        print("File name: ", filename)
+                        print("Line number: ", line_number)
+                        #print("ERROR3: - Something went wrong when reading the WORK file: " + str(work_fname) + " (" + str( e) + ")")
 
-                        if ann_obj is not None and ann_obj.df_ is not None:
-                            df = ann_obj.df_
-                            #print(df)
-
-                            #print( self.pq_filepath)
-                            #print(  self.pq_writer)
-                            self.pq_writer = self.append_to_parquet(df, self.pq_filepath, self.pq_writer)
-                            print (raw_fname)
+                    if ann_obj is not None and ann_obj.df_ is not None:
+                        df = ann_obj.df_
+                        #print(df)
+                        #print( self.pq_filepath)
+                        #print(  self.pq_writer)
+                        self.pq_writer = self.append_to_parquet(df, self.pq_filepath, self.pq_writer)
+                        print (raw_fname)
 
 
         self.pq_writer.close()
@@ -118,5 +150,7 @@ if __name__ == '__main__':
             workdir = sys.argv[sys.argv.index("-workdir") + 1]
         if "-savefile" in sys.argv:
             pq_filepath = sys.argv[sys.argv.index("-savefile") + 1]
-        parser = ParseWorkFiles(rawdir=rawdir, workdir=workdir, pq_filepath=pq_filepath)
+        if "-svzarr" in sys.argv:
+            svzarr_file = sys.argv[sys.argv.index("-svzarr") + 1]
+        parser = ParseWorkFiles(rawdir=rawdir, workdir=workdir, pq_filepath=pq_filepath,svzarr_file=svzarr_file)
         parser.run()
