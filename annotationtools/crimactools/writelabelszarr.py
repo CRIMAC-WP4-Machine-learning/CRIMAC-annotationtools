2e22e5fed64c314f0288cb18b589cd8a9f254c1e
Tomasz Furmanek <88382277+tomaszfurmanek@users.noreply.github.com>
2023-10-19 14:31:19 +0200

    Update writelabelszarr.py

--- annotationtools/crimactools/writelabelszarr.py
+++ annotationtools/crimactools/writelabelszarr.py
@@ -245,12 +245,12 @@ class WriteLabelsZarr:
         )
         git_rev = os.getenv('COMMIT_SHA', 'XXXXXXXX')
         print(git_rev)
-        try:
-            git_rev =  subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
-        except Exception as e:
-            git_rev = os.getenv('COMMIT_SHA', 'XXXXXXXX')
-            print("error getting git revision")
-        print(git_rev)
+        #try:
+        #    git_rev =  subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+        #except Exception as e:
+        #    git_rev = os.getenv('COMMIT_SHA', 'XXXXXXXX')
+        #    print("error getting git revision")
+        #print(git_rev)
         # Append version attributes
         ds4.attrs = dict(
             name = "CRIMAC-labels",
