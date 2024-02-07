c8c47b1336356c252417710452054707a5e37381
Benoit Berges <benoit.berges@wur.nl>
2024-02-07 21:07:42 +0100

    change package setup to *.toml

--- annotationtools/crimactools/__init__.py
+++ annotationtools/crimactools/__init__.py
@@ -1,3 +1,4 @@
 from .correct_distping import correct_parquet, isNaN, fill_nan
 from .parseworkfiles import ParseWorkFiles
 from .writelabelszarr import WriteLabelsZarr
+from .CRIMAC_makelabelszarr import writelabels
